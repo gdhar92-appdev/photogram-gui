52582b6f75db10655ba8c78dce691629c1dd6c58
gdhar92 <gdhar@chicagobooth.edu>
2021-05-11 07:37:14 +0000

    6th commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,25 @@ class PhotosController < ApplicationController
 
   def index
   
+    matching_photos = Photo.all
+    
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
+
     render({:template => "photo_templates/index.html.erb"})
+
+  end
+
+  def show
+    
+    url_photo_id = params.fetch("path_photo_id").to_i
+    matching_photos = Photo.where({ :id => url_photo_id})
+    @the_photo = matching_photos.first
+    
+    if @the_photo == nil
+      redirect_to("/404")
+    else
+      render({ :template => "photo_templates/show.html.erb"})
+    end
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
 
   get("/photos", { :controller => "photos", :action => "index"})
 
+  get("/photos/:path_photo_id", { :controller => "photos", :action => "show"})
+
 end
