984d1aba141d3876c4b38c2bcef2ddec25689cc2
gdhar92 <gdhar@chicagobooth.edu>
2021-05-13 01:19:17 +0000

    7th commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -24,4 +24,15 @@ class PhotosController < ApplicationController
 
   end
 
+  def delete_photo
+
+    url_photo_id = params.fetch("path_photo_id").to_i
+    matching_photos = Photo.where({ :id => url_photo_id})
+    the_photo = matching_photos.first
+    the_photo.destroy
+    redirect_to("/photos")
+    #render({ :template => "photo_templates/delete.html.erb"})
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   get("/photos/:path_photo_id", { :controller => "photos", :action => "show"})
 
+  get("/delete_photo/:path_photo_id", { :controller => "photos", :action => "delete_photo"})
 end
